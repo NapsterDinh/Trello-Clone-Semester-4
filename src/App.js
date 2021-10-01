489d517aad0c39ce13d9d15a9db1a6a7364d0b0e
TanTu Dinh <tantudinh1@gmail.com>
2021-10-01 19:47:38 +0700

    Design Trello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="trello-app">
+        <nav className="navbar app">App bar</nav>
+        <nav className="navbar board">Board Bar</nav>
+        <div className="board-columns">
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brain Storm</header>
+            <ul>
+              <img src="https://chephamvisinh.vn/wp-content/uploads/2020/07/Cach-khu-mui-hoi-cho-meo-hieu-qua-nhat.jpg" alt=""></img>
+              <li>Title: Trello Clone</li>
+              <li>Add y our what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+              <li>Add your what you'd like to do below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+        </div>
+      </div>
   );
 }
 
